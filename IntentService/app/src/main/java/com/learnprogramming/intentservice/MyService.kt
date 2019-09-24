f992c12be8492f075b0e4a725b39e8982efb4ec7
panha <khepanha18@kit.edu.kh>
2019-09-24 23:49:05 +0700

    :books:New Commit

---
+++ IntentService/app/src/main/java/com/learnprogramming/intentservice/MyService.kt
@@ -0,0 +1,23 @@
+package com.learnprogramming.intentservice
+
+import android.app.IntentService
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.widget.Toast
+
+class MyService : IntentService("daemon") {
+    override fun onHandleIntent(p0: Intent?) {
+        Thread.sleep(10000)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "Download Service...",Toast.LENGTH_LONG).show()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Service Destroyed!",Toast.LENGTH_LONG).show()
+    }
+}
